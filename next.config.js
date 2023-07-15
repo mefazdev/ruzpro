4731d77b8f38e1aa26edcd22d92e7e661e4e6dc5
mefazdev <fazdev.com@gmail.com>
2023-07-15 10:22:21 +0530

    pwa-7

--- next.config.js
+++ next.config.js
@@ -4,42 +4,34 @@
 //   swcMinify: true,
 // }
 
-module.exports = {
-  reactStrictMode: true,
-};
+ 
 
-module.exports = {
-  reactStrictMode: true,
-};
+module.exports = { reactStrictMode: true };
 const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
     register: true,
-        disable: process.env.NODE_ENV ===      'development',
+    disable: process.env.NODE_ENV === "development",
     skipWaiting: true,
   },
 });
+
 module.exports = {
-    reactStrictMode: true,
-  env:{
-    MONGO_URI : 'mongodb+srv://ruzpro:h9TgtxA2sdou5Ejn@cluster0.kozruih.mongodb.net/ruzpro?retryWrites=true&w=majority'
+  reactStrictMode: true,
+  env: {
+    MONGO_URI:
+      "mongodb+srv://ruzpro:h9TgtxA2sdou5Ejn@cluster0.kozruih.mongodb.net/ruzpro?retryWrites=true&w=majority",
+  },
 
-  }
+  // env:{
+  //   MONGO_URI : 'mongodb+srv://salah:wD5wxV3sek4BPTKJ@cluster0.dzvfwsq.mongodb.net/?retryWrites=true&w=majority'
 
-    
-
-  
-
-// env:{
-//   MONGO_URI : 'mongodb+srv://salah:wD5wxV3sek4BPTKJ@cluster0.dzvfwsq.mongodb.net/?retryWrites=true&w=majority'
-
-// }
-}
+  // }
+};
 // mongodb+srv://ruzpro:<password>@cluster0.kozruih.mongodb.net/?retryWrites=true&w=majority
 
 // h9TgtxA2sdou5Ejn
-
 
 // env:{
 //   MONGO_URI : 'mongodb+srv://salah:wD5wxV3sek4BPTKJ@cluster0.dzvfwsq.mongodb.net/?retryWrites=true&w=majority'
